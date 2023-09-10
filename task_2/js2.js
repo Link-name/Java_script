85e27510a250d6b66a8e4dceb09d21b035c48338
Link <stereo_house01@mail.ru>
2023-09-10 14:21:28 +0300

    dz_2 js

---
+++ task_2/js2.js
@@ -0,0 +1,13 @@
+
+/* const youAge = Number(prompt('Какой вашь возраст')); // всегда стритк возвращает 
+
+console.log(typeof(youAge));
+console.log(youAge); */
+/* a = 1%2;
+console.log(a);
+console.log(['a'])
+console.log(Number([1]))
+console.log(typeof(Number(['a'])))
+console.log(typeof(Number([1]))) */
+
+console.log(1/0===1/-0)
